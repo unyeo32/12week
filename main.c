4a1a722f4ee950109f582609748bc9858939857d
unyeo32 <unyeo32@sm.ac.kr>
2020-11-19 15:43:59 +0900

    exercise3.4

--- main.c
+++ main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	int i=0;
-	char str[4];
-	
-	str[0]='a';
-	str[1]='b';
-	str[2]='c';
-	str[3]='\0';
-	
-	printf("%s\n", str);
-	
-	while(str[i] != '\0'){
-		printf("%c\n", str[i]);
-		i++;
-	}
-	return 0;
+char src[]= "the worst things to eat before you sleep";
+char dst[100];
+char str[30] = "happy c programming";
+
+strcpy(dst, src);
+
+printf("copied string: %s\n", dst);
+printf("length of \"%s\": %i\n", str, strlen(str));
+
+return 0;
 }
