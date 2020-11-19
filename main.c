6af15ec99f5a65db5941ef1a367b42040c480dbd
unyeo32 <unyeo32@sm.ac.kr>
2020-11-19 15:56:23 +0900

    exercise5

--- main.c
+++ main.c
@@ -5,15 +5,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int i;
+	FILE *fp;
+	char str[100];
 	
-char src[]= "the worst things to eat before you sleep";
-char dst[100];
-char str[30] = "happy c programming";
-
-strcpy(dst, src);
-
-printf("copied string: %s\n", dst);
-printf("length of \"%s\": %i\n", str, strlen(str));
+	fp = fopen("sample.txt", "w");
+	if (fp == NULL)
+	{
+		return -1;
+	}
+	for (i=0;i<3; i++){
+	
+		printf("Input a word : ");
+		scanf("%s", str);
+		fprintf(fp, "%s\n", str);
+	}
+	
+	fclose(fp);
 
 return 0;
 }
