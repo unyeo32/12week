321af58b55a87d22dd3c3c7d4cfa45e3f5b97cdf
unyeo32 <unyeo32@sm.ac.kr>
2020-11-19 16:18:33 +0900

    exercise6

--- main.c
+++ main.c
@@ -5,20 +5,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
+
 	FILE *fp;
+	//char c;
 	char str[100];
 	
-	fp = fopen("sample.txt", "w");
+	fp = fopen("sample.txt", "r");
 	if (fp == NULL)
 	{
 		return -1;
 	}
-	for (i=0;i<3; i++){
 	
-		printf("Input a word : ");
-		scanf("%s", str);
-		fprintf(fp, "%s\n", str);
+	//while ( (c=fgetc(fp)) != EOF){
+	while ( fgets(str,100, fp) != NULL){
+	
+		//printf("%c", c);
+		printf("%s", str);
 	}
 	
 	fclose(fp);
